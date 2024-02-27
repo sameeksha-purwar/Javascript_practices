7a8d96fdc9eaf6519e0d5c12b125bb25567e33a4
Sameeksha purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2024-02-27 14:40:20 +0000

    dataTypes

---
+++ Javascript_Part1/DataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; 
+
+
+console.log(3 + 3) 
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+console.log(typeof undefined); 
+console.log(typeof null); 
